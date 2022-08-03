f217e84b884dab0e242b72504934bb319f7c4c12
s8189882 <s8189882@gmail.com>
2022-08-03 13:18:07 +0530

    Calculating monthly wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,13 +1,13 @@
-
-#
-#Start with Displaying Welcome to Employee Wage Computation Program on Master #Branch
-#
-#
+#!/bin/bash -x
 
 
 echo ""
+
+#Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
 echo "Welcome to Employee Wage Computation Program "
 
+
+#Check Employee is Present or Absent
 attendance=$(($((RANDOM%2))+1))
 h=8
 
@@ -17,7 +17,11 @@ else
 	echo "Employee is absent."
 fi
 
-wage=$((h*20))  #daily wage = 20 times number of hours #Full day=8hours
+#Calculate daily wage = 20 times number of hours #Full day=8hours
+dailyWage=$((h*20))  
+
+
+#Solving using Switch Case Statement
 
 isPartTime=1;
 isFullTime=2;
@@ -27,14 +31,22 @@ empCheck=$(($((RANDOM%2))+1));
 case $empCheck in
 	$isFullTime)
 		h=8
+		echo "Employee is a Full time employee"
 		;;
 	$isPartTime)
 		h=8
-		;;
-	*)
-		h=0
+		echo "Employee is a Part time employee"
 		;;
 esac
+
+#Calculating Wages for a Month
+#Assume 20 Working Day per Month
+
+dailyWage=$((h*20))
+monthlyWage=$((dailyWage*20))
+
+echo "Daily wage : $ $dailyWage"
+echo "Monthly wage : $ $monthlyWage"
 
 
 echo ""
