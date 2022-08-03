74fe90d35655723a14c5183bd36af59e42423c5e
s8189882 <s8189882@gmail.com>
2022-08-03 12:15:37 +0530

    Check whether employee is present or absent

--- employeeWage.sh
+++ employeeWage.sh
@@ -7,4 +7,14 @@
 
 echo ""
 echo "Welcome to Employee Wage Computation Program "
+
+
+
+attendance=$(($((RANDOM%2))+1))
+
+if [[ $attendance -eq 1 ]]; then
+	echo "Employee is present."
+else
+	echo "Employee is absent."
+fi
 echo ""
