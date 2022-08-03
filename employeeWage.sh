de398650b3386e750a0c1d49a304d2105e23a287
s8189882 <s8189882@gmail.com>
2022-08-03 14:04:25 +0530

    Refactoring the Code to write a function to get working hours

--- employeeWage.sh
+++ employeeWage.sh
@@ -8,49 +8,47 @@ echo "Welcome to Employee Wage Computation Program "
 echo ""
 
 
-#Calculate Wages till a condition of total working hours or days is reached for a month
-#Assume 100 hours and 20 days
+#Refactor the Code to write a function to get work hours
+
+function calculateWorkingHour() {
+	case $1 in
+		0)
+			workingHour=0;
+			;;
+
+		1)
+			workingHour=8;
+			;;
+		2)
+			workingHour=4;
+			;;
+	esac;
+	echo $workingHour;
+}
 
 perHourSalary=20;
-workingHour=0;
 totalSalary=0;
 totalWorkingHour=0;
-day=1
+day=1;
 
 while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
-	((day++));
-	isPresent=$((RANDOM%3));
-	case $isPresent in
-		0)
-		echo "Employee is absent";
-		workingHour=0;
-		;;
-
-		1)
-		echo "Employee is working full time";
-		workingHour=8;
-		;;
-
-		2)
-		echo "Employee is working part time";
-		workingHour=8;
-		;;
-	esac
-	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	wHour=$(calculateWorkingHour $((RANDOM%3)));
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
 	if [ $totalWorkingHour -gt 100 ]
 	then
-		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
 		break;
 	fi
-	salary=$(($perHourSalary * $workingHour));
+	salary=$(($perHourSalary * $wHour));
 	totalSalary=$(($totalSalary + $salary));
+	((day++));
 done
 
 
 echo "Days worked : $day"
-echo "Total working hours : $totalWorkingHour"
-echo "Salary for the month : $totalSalary"
+echo "Total working hours : $totalWorkingHour hours"
+echo "Salary for the month : $ $totalSalary"
 
 
 echo ""
